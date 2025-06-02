bcc1041dac274f2d0cfc4aebd529e5621aca61bb
DEVSHARMA <145190056+Devsharma8875@users.noreply.github.com>
2025-06-02 10:26:16 +0530

    Update homepagePlaylists.js

--- src/utils/homepagePlaylists.js
+++ src/utils/homepagePlaylists.js
@@ -1,22 +1,28 @@
-export const homepagePlaylistInfo = [
-  {
-    title: "Trending Now",
-    id: "OLAK5uy_lSTp1DIuzZBUyee3kDsXwPgP25WdfwB40",
-  },
+export const latestPlaylists = [
+  { title: "New Released", id: "RDCLAK5uy_ksEjgm3H_7zOJ_RHzRjN1wY-_FFcs7aAU" },
   {
     title: "New Music Hindi",
     id: "RDCLAK5uy_nNhhgRET3NcJ4SJBvqhAIJ6t7vjsQYowc",
   },
   {
-    title: "Bollywood Hits",
-    id: "RDCLAK5uy_n9Fbdw7e6ap-98_A-8JYBmPv64v-Uaq1g",
+    title: "New Indian Pop",
+    id: "RDCLAK5uy_k66J6mE65JgdE4zoeNSzmw_16JB_ueINE",
   },
   {
-    title: "90s Bollywood Romance",
-    id: "RDCLAK5uy_lnm4v4arFrmL63NUzIdoXJe-E7G4_sriU",
+    title: "New Music Punjab",
+    id: "RDCLAK5uy_mk3xwsayv9PxawuXS-U6ao9eMeNmSwYAM",
   },
   {
-    title: "Motivational Tracks",
-    id: "RDCLAK5uy_nyta-kQ65CIZc84rEbmNVk7y2oZt4HB1A",
+    title: "New Music Haryanvi",
+    id: "RDCLAK5uy_nTkyDVpCk3iCQG_3bDJyhGgb1uzcBZM4A",
+  },
+  {
+    title: "New Music Telgu",
+    id: "RDCLAK5uy_l8CaYQvBQWVT2st1VsW9JjODWisR_vd3U",
+  },
+  {
+    title: "New Music Tamil",
+    id: "RDCLAK5uy_nVQAtE2KBWk-ROQIc5o39Oup3hOLnYV0g",
   },
 ];
+
